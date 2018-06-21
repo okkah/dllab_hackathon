f4c9eaa41bbd695c7f82c24368a55c59c71240bc
okkah <trombonehide@yahoo.co.jp>
2018-06-21 22:56:04 +0900

    fix typo

--- dumplabel.py
+++ dumplabel.py
@@ -3,6 +3,31 @@ import os
 import numpy as np
 import pickle
 
+# train
+img_shape=(224,224)
+imgs = []
+for filename in os.listdir("../data/train/images"):
+    img = Image.open("../data/train/images/" + filename)
+    img = img.resize(img_shape)
+    img = np.asarray(img, dtype='f')
+    img /= 255
+    img = np.transpose(img, (2, 0, 1))
+    imgs.append((filename, img))
+    
+labels = {}
+with open("../data/train/train_labels.txt", "r") as f:
+    for line in f:
+        labels[line.split()[0]] = line.split()[1]    
+
+train = []
+for fn, img in imgs:
+    train.append((img, np.int8(labels[fn])))
+
+with open("traindata.pickle", "wb") as f:
+    pickle.dump(train, f)
+
+
+# test
 img_shape=(224, 224)
 imgs = []
 for filename in os.listdir("../data/valid/images"):
@@ -25,25 +50,4 @@ for fn, img in imgs:
 with open("../testdata.pickle", "wb") as f:
     pickle.dump(test, f)
 
-img_shape=(224,224)
 
-imgs = []
-for filename in os.listdir("../data/train/images"):
-    img = Image.open("../data/train/images/" + filename)
-    img = img.resize(img_shape)
-    img = np.asarray(img, dtype='f')
-    img /= 255
-    img = np.transpose(img, (2, 0, 1))
-    imgs.append((filename, img))
-    
-labels = {}
-with open("../data/train/train_labels.txt", "r") as f:
-    for line in f:
-        labels[line.split()[0]] = line.split()[1]    
-
-train = []
-for fn, img in imgs:
-    train.append((img, np.int8(labels[fn])))
-
-with open("traindata.pickle", "wb") as f:
-    pickle.dump(train, f)
